b385cbced0ab475aa384879a6b3e12f60399961b
King Kay Dee <kwekudeheer@ymail.com>
2021-02-17 22:48:12 +0000

    WIP to make more configurable

--- Source/OrganizeDownloadFiles.py
+++ Source/OrganizeDownloadFiles.py
@@ -5,9 +5,49 @@ import shutil
 import os
 import json
 import time
+import xml.etree.ElementTree as ET
 
 # Impl
+class FolderConfig:
+    def __init__(self):
+        self.ReadConfig()
 
+    def ReadConfig(self):
+        cwd = os.getcwd()
+        file = open(cwd + "/Paths.ini", "r")
+        lines = file.readlines();
+        for line in lines:
+            line.replace(" ", "")
+            split = line.split(":", 1)
+            key = split[0]
+            path = split[1]
+
+            if key.lower() == "source":
+                source = path
+            else:
+                destination = path
+
+    source = str()
+    destination = str()
+
+class RedirectConfig:
+    redirects = dict()
+            
+    def __init__(self):
+        self.ReadConfig()
+
+    def ReadConfig(self):
+        cwd = os.getcwd()
+        file = ET.parse(cwd + "/Redirect.xml")
+        root = file.getroot()
+        extList = list()
+        for folderItem in root.findall("Folder"):
+            folder = folderItem.attrib["name"]
+            for extItem in folderItem.findall("Extensions"):
+                ext = extItem.text()
+                extList.append(ext)
+            redirects[folder] = extList
+        
 
 class MyHandler(FileSystemEventHandler):
     def MoveFile(self, src, dest, copyNum):
@@ -31,8 +71,8 @@ class MyHandler(FileSystemEventHandler):
 
 
     def on_modified(self, event):
-        for filename in os.listdir(sourceFolder):
-            srcFilepath = sourceFolder + "/" + filename
+        for filename in os.listdir(FolderConfig.source):
+            srcFilepath = FolderConfig.source + "/" + filename
             destDirectory = str()
 
             for (path, exts) in destFolders.items():
@@ -46,25 +86,22 @@ class MyHandler(FileSystemEventHandler):
                 destPath = destDirectory + "/" + filename
                 self.MoveFile(srcFilepath, destPath, 1)
 
-
-sourceFolder = "C:/Users/44753/Downloads"
-destFolders = {
-    "C:/Users/44753/Downloads/Pictures": [".png", ".jpeg", ".jpg"],
-    "C:/Users/44753/Downloads/Apps": [".exe", ".msi"],
-}
-
 # Main
 event_handler = MyHandler()
 
+folderConfig = FolderConfig()
+redirectConfig = RedirectConfig()
+
 observer = Observer()
-observer.schedule(event_handler, sourceFolder, recursive=True)
+observer.schedule(event_handler, FolderConfig.source, recursive=True)
 observer.start()
 
 try:
-    for (path, exts) in destFolders.items():
+    for (path, exts) in redirectConfig.items():
+        dest = "{}/{}".format(folderConfig.destination, path)
         for ext in exts:
-            os.makedirs(path, exist_ok=True)
-            print("File redirect: {} -> {}".format(ext, path))
+            os.makedirs(dest, exist_ok=True)
+            print("File redirect: {} -> {}".format(ext, dest))
 
     print("Entering loop")
     while True:
