0a4acc763ae6b81966846741de3a72f2060537f8
King Kay Dee <kwekudeheer@ymail.com>
2021-02-18 04:48:36 +0000

    Code clean up and allowing commenting to work

--- Source/FolderOrganizer.py
+++ Source/FolderOrganizer.py
@@ -12,25 +12,78 @@ cwd = os.getcwd()
 configFolder = cwd + "/../Configurations"
 
 
+class ConfigPair:
+    def __init__(self, k: str, v):
+        self.key = k.lower()
+        self.value = v
+
+    key: str
+    value: str
+
+class ConfigFileReader:
+    @staticmethod
+    def Get(path, cb):
+        print("Reading configurations: " + path)
+        file = open(path, "r")
+        lines = file.readlines()
+        for line in lines:
+            line = line.split("*", 1)[0]
+            line = line.strip()
+            if line == "":
+                continue
+
+            split = line.split(":", 1)
+            key = split[0]
+            value = split[1]
+            key = key.replace(" ", "")
+            value = value.replace(" ", "")
+            value = value.strip()
+            config = ConfigPair(key.lower(), value)
+            cb(config)
+        file.close()
+
+class InternalConfig:
+    def __init__(self):
+        self.ReadConfig()
+
+    def SetUpdateRate(self, cfg: ConfigPair):
+        self.updateRate = int(cfg.value)
+
+    def ReadConfig(self):
+        print("Internals Configurations")
+        ConfigFileReader.Get(configFolder + "/Internals.ini",
+                             self.SetUpdateRate)
+
+    def Update(self):
+        self.ReadConfig()
+
+    updateRate: int
+
+
 class FolderConfig:
     def __init__(self):
         self.ReadConfig()
 
-    def ReadConfig(self):
-        file = open(configFolder + "/Paths.ini", "r")
-        lines = file.readlines()
-        for line in lines:
-            split = line.split(":", 1)
-            key = split[0]
-            path = split[1]
-            key = key.replace(" ", "")
-            path = path.replace(" ", "")
-            path = path.strip()
+    def AssignPathCallback(self, config: ConfigPair):
+        if not config.key.lower().find("source") == -1:
+            self.SetSource(config.value)
+        else:
+            self.SetDestination(config.value)
 
-            if key.lower() == "source":
-                self.source = path
-            else:
-                self.destination = path
+    def ReadConfig(self):
+        print("Folder Configurations")
+        self.source = str()
+        self.destination = str()
+        ConfigFileReader.Get(configFolder + "/Paths.ini",
+                             self.AssignPathCallback)
+
+    def SetSource(self, path):
+        self.source = path
+        print("Tracking source path: " + self.source)
+
+    def SetDestination(self, path):
+        self.destination = path
+        print("Destination path: " + self.destination)
 
     source = str()
     destination = str()
@@ -41,17 +94,28 @@ class RedirectConfig:
         self.ReadConfig()
 
     def ReadConfig(self):
-        file = ET.parse(configFolder + "/Redirects.xml")
-        root = file.getroot()
-        for folderItem in root.findall("Folder"):
-            extList = []
-            folder = folderItem.attrib["name"]
-            for extsItem in folderItem.findall("Extensions"):
-                for extItem in extsItem.findall("Extension"):
-                    ext = extItem.text
-                    extList.append(ext)
-            self.redirects[folder] = extList
+        print("File redirect Configurations")
+        try:
+            elemTree = ET.parse(configFolder + self.settingsFile)
+            self.redirects = {}
+            root = elemTree.getroot()
+            for folderItem in root.findall("Folder"):
+                extList = []
+                folder = folderItem.attrib["name"]
+                print("Folder: " + folder)
+                for extsItem in folderItem.findall("Extensions"):
+                    for extItem in extsItem.findall("Extension"):
+                        ext = extItem.text
+                        extList.append(ext)
+                        print("  - Extension: " + ext)
+                self.redirects[folder] = extList
+        except:
+            print("Problem reading file: " + self.settingsFile)
 
+    def Update(self):
+        self.ReadConfig()
+
+    settingsFile = "/Redirects.xml"
     redirects = {}
 
 
@@ -73,18 +137,19 @@ class MyHandler(FileSystemEventHandler):
                 dest = "{}{}{}".format(pathNoExt, copyNum, ext)
                 self.MoveFile(src, dest, copyNum + 1)
         except:
-            print("Error: unable to move file: " + src)
+            print("[Error] unable to move file: " + src)
 
     def on_modified(self, event):
-        for filename in os.listdir(folderConfig.source):
-            srcFilepath = folderConfig.source + "/" + filename
+        for filename in os.listdir(folderCfg.source):
+            srcFilepath = folderCfg.source + "/" + filename
             destDirectory = str()
 
             found = False
-            for (path, exts) in redirConfifg.redirects.items():
+            for (path, exts) in redirCfg.redirects.items():
                 for ext in exts:
                     if (filename.lower().endswith(ext)):
-                        destDirectory = "{}/{}".format(folderConfig.destination, path)
+                        destDirectory = "{}/{}".format(
+                            folderCfg.destination, path)
                         print("Extension match: '{}' ".format(ext))
                         print("Path: '{}'".format(srcFilepath))
 
@@ -96,28 +161,24 @@ class MyHandler(FileSystemEventHandler):
                     break
 
 
-
-
 # Main
 event_handler = MyHandler()
 
-folderConfig = FolderConfig()
-redirConfifg = RedirectConfig()
+internalCfg = InternalConfig()
+folderCfg = FolderConfig()
+redirCfg = RedirectConfig()
 
 observer = Observer()
-observer.schedule(event_handler, folderConfig.source, recursive=True)
+observer.schedule(event_handler, folderCfg.source, recursive=True)
 observer.start()
 
 try:
-    for (path, exts) in redirConfifg.redirects.items():
-        dest = "{}/{}".format(folderConfig.destination, path)
-        for ext in exts:
-            os.makedirs(dest, exist_ok=True)
-            print("File redirect: {} -> {}".format(ext, dest))
-
     print("Entering loop")
+
     while True:
-        time.sleep(10)
+        time.sleep(int(internalCfg.updateRate))
+        redirCfg.Update()
+        internalCfg.Update()
     print("Loop terminated")
 except:
     observer.stop()
